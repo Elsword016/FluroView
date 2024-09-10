32cfaa12143b9cd52fc7335548fc4d55808ccebb
Biswanath Saha <bsaha0659@gmail.com>
2024-09-10 17:50:51 +0900

    Update __init__.py

--- src/FluroView/__init__.py
+++ src/FluroView/__init__.py
@@ -1,4 +1,4 @@
-from bioviz_multichannel import MultiChannelImage
-from bioviz_ImagePanel import ImagePanel
+from .bioviz_multichannel import MultiChannelImage
+from .bioviz_ImagePanel import ImagePanel
 
 __all__ = ['MultiChannelImage', 'ImagePanel']
